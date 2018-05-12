f5823bcd0a3dc384c28b8ce12e824a358b9dd29c
Duncan <loxa10077@gmail.com>
2018-05-12 21:11:21 +0800

    optimize method completed?

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,8 @@ class Item < ApplicationRecord
   belongs_to :user
 
   def completed?
-    !completed_at.blank?
-    # completed_at.present?
+    completed_at.present?
+    # !completed_at.blnck?
     # 沒驚嘆號代表是空的話就顯示ture
     # 加驚嘆號代表不是空的話(裡面有東西)就顯示ture
   end
